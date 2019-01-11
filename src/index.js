ad8196dd4e9ad338659bd4ea73476cb7d992db81
jijothic <george.jijothic@gmail.com>
2019-01-11 12:11:55 +0300

    Added the graphql server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { createServer } from 'http';
 import bodyParser from 'body-parser';
 
-const app = express(); // create an instance of express
+import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 
-const PORT = process.env.PORT || 3000; // create the port
+app.use(bodyParser.json());
 
-app.use(bodyParser.json()); // add body-parser as the json parser middleware
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: constants.GRAPHQL_PATH,
+  }),
+);
 
-app.listen(PORT, err => {
+app.use(
+  constants.GRAPHQL_PATH,
+  graphqlExpress({
+    schema
+  }),
+);
+
+graphQLServer.listen(constants.PORT, err => {
   if (err) {
     console.error(err);
   } else {
-    console.log(`App listen on port: ${PORT}`);
+    console.log(`App running on port: ${constants.PORT}`);
   }
 });
