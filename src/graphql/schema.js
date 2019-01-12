3db80fcfee9ac26eee5dfcbc435aab8593b98312
jijothic <george.jijothic@gmail.com>
2019-01-12 16:47:58 +0300

    Added schema

---
+++ src/graphql/schema.js
@@ -0,0 +1,10 @@
+type User {
+    _id: ID!
+    username: String
+    email: String!
+    firstName: String
+    lastName: String
+    avatar: String
+    createdAt: Date!
+    updatedAt: Date!
+  }
