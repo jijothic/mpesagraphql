d1939ba145f9a2a350c8172cdda7d9b328c2bddd
jijothic <george.jijothic@gmail.com>
2019-01-14 13:56:50 +0300

    Updated graphql schema

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -7,4 +7,11 @@ type User {
     avatar: String
     createdAt: Date!
     updatedAt: Date!
+  },
+  type Mutation {
+    createTweet(text: String!): Tweet
+    updateTweet(_id: ID!, text: String): Tweet
+    deleteTweet(_id: ID!): Status
+    signup(email: String!, fullName: String!, password: String!, avatar: String, username: String): User
+    login(email: String!, password: String!): User
   }
