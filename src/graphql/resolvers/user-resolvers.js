386ecb22a3f36e4cbe521c541df07f808bc1615a
jijothic <george.jijothic@gmail.com>
2019-01-14 13:39:52 +0300

    Update user resolver

--- src/graphql/resolvers/user-resolvers.js
+++ src/graphql/resolvers/user-resolvers.js
@@ -18,4 +18,22 @@ export default {
             avatar
         });
     },
+    login: async (_, {
+        email,
+        password
+    }) => {
+        const user = await User.findOne({
+            email
+        });
+
+        if (!user) {
+            throw new Error('User not exist!');
+        }
+
+        if (!user.authenticateUser(password)) {
+            throw new Error('Password not match!');
+        }
+
+        return user;
+    }
 }
