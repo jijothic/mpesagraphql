7ef7fe8f6f8e7fed053f7a334fc8b20bf4ddcc86
jijothic <george.jijothic@gmail.com>
2019-01-14 13:58:50 +0300

    Added jwt token

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,6 @@
 export default {
     PORT: process.env.PORT || 3000,
     DB_URL: 'mongodb://localhost/mpesa-development',
-    GRAPHQL_PATH: '/graphql'
+    GRAPHQL_PATH: '/graphql',
+    JWT_SECRET: 'thisisasecret123'
   }
