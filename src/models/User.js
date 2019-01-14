13086c005a7e5783e715369c9da40a41f79d0613
jijothic <george.jijothic@gmail.com>
2019-01-14 13:31:39 +0300

    Added bcrypt to user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,30 @@
-import mongoose, {
-    Schema
-} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import { hashSync } from 'bcrypt-nodejs';
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-    },
-    firstName: String,
-    lastName: String,
-    avatar: String,
-    password: String,
-    email: String,
-}, {
-    timestamps: true
+  username: {
+    type: String,
+    unique: true,
+  },
+  firstName: String,
+  lastName: String,
+  avatar: String,
+  password: String,
+  email: String,
+}, { timestamps: true });
+
+UserSchema.pre('save', function(next) {
+  if (this.isModified('password')) {
+    this.password = this._hashPassword(this.password);
+    return next();
+  }
+  return next();
 });
+
+UserSchema.methods = {
+  _hashPassword(password) {
+    return hashSync(password);
+  },
+}
 
 export default mongoose.model('User', UserSchema);
