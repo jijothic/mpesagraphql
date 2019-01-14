13f48bfba781ea655adf22f117c8bacede30b376
jijothic <george.jijothic@gmail.com>
2019-01-14 15:02:01 +0300

    Updated user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,33 +1,45 @@
 import mongoose, { Schema } from 'mongoose';
 import { hashSync, compareSync } from 'bcrypt-nodejs';
+import jwt from 'jsonwebtoken';
+
+import constants from '../config/constants';
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-    },
-    firstName: String,
-    lastName: String,
-    avatar: String,
-    password: String,
-    email: String,
+  username: {
+    type: String,
+    unique: true
+  },
+  firstName: String,
+  lastName: String,
+  avatar: String,
+  password: String,
+  email: String
 }, { timestamps: true });
 
-UserSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
-        this.password = this._hashPassword(this.password);
-        return next();
-    }
+UserSchema.pre('save', function(next) {
+  if (this.isModified('password')) {
+    this.password = this._hashPassword(this.password);
     return next();
+  }
+
+  return next();
 });
 
 UserSchema.methods = {
-    _hashPassword(password) {
-        return hashSync(password);
-    },
-    authenticateUser(password) {
-        return compareSync(password, this.password);
-    },
-};
+  _hashPassword(password) {
+    return hashSync(password);
+  },
+  authenticateUser(password) {
+    return compareSync(password, this.password);
+  },
+  createToken() {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      constants.JWT_SECRET,
+    );
+  },
+}
 
 export default mongoose.model('User', UserSchema);
