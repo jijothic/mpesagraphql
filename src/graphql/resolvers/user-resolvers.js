ba02253d7eec9331872cffd3ad00e38af22f8157
jijothic <george.jijothic@gmail.com>
2019-01-18 17:34:00 +0300

    updated the user resolver

--- src/graphql/resolvers/user-resolvers.js
+++ src/graphql/resolvers/user-resolvers.js
@@ -1,39 +1,61 @@
 import User from '../../models/User';
+import {
+    requireAuth
+} from '../../services/auth';
 
 export default {
-    signup: (_, {
+    signup: async (_, {
         fullName,
-        username,
-        password,
-        email,
-        avatar
+        ...rest
     }) => {
         const [firstName, ...lastName] = fullName.split(' ');
-        return User.create({
-            firstName,
-            lastName,
-            username,
-            password,
-            email,
-            avatar
-        });
+        try {
+            const user = await User.create({
+                firstName,
+                lastName,
+                ...rest
+            });
+            return {
+                token: user.createToken(),
+            };
+        } catch (error) {
+            throw error;
+        }
     },
+
     login: async (_, {
         email,
         password
     }) => {
-        const user = await User.findOne({
-            email
-        });
+        try {
+            const user = await User.findOne({
+                email
+            });
 
-        if (!user) {
-            throw new Error('User not exist!');
+            if (!user) {
+                throw new Error('User not exist!');
+            }
+
+            if (!user.authenticateUser(password)) {
+                throw new Error('Password not match!');
+            }
+
+            return {
+                token: user.createToken(),
+            };
+        } catch (error) {
+            throw error;
         }
+    },
 
-        if (!user.authenticateUser(password)) {
-            throw new Error('Password not match!');
+    me: async (_, args, {
+        user
+    }) => {
+        try {
+            const me = await requireAuth(user);
+            return me;
+        } catch (error) {
+            throw error;
         }
-
-        return user;
-    }
-}
+    },
+};
