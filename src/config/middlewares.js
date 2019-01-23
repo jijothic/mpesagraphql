63df4914d58b3b1e911c6d0998cff7ab7d087a25
jijothic <george.jijothic@gmail.com>
2019-01-23 11:15:55 +0300

    updates the middlewares js

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -10,34 +10,14 @@ import {
 import constants from './constants';
 import typeDefs from '../graphql/schema';
 import resolvers from '../graphql/resolvers';
-import {
-    decodeToken
-} from '../services/auth';
 
 const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
 });
- 
-
-async function auth(req, res, next) {
-    try {
-        const token = req.headers.authorization;
-        if (token != null) {
-            const user = await decodeToken(token);
-            req.user = user; // eslint-disable-line
-        } else {
-            req.user = null; // eslint-disable-line
-        }
-        next();
-    } catch (error) {
-        throw error;
-    }
-}
 
 export default app => {
     app.use(bodyParser.json());
-    app.use(auth);
     app.use(
         '/graphiql',
         graphiqlExpress({
@@ -46,11 +26,11 @@ export default app => {
     );
     app.use(
         constants.GRAPHQL_PATH,
-        graphqlExpress(req => ({
+        graphqlExpress({
             schema,
             context: {
                 user: req.user,
             },
-        })),
+        }),
     );
 };
