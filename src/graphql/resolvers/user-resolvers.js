a17d5d9587a29bb3bdf5baa72a3967f0fa1f67b5
jijothic <george.jijothic@gmail.com>
2019-01-24 12:24:50 +0300

    updated user resolver

--- src/graphql/resolvers/user-resolvers.js
+++ src/graphql/resolvers/user-resolvers.js
@@ -1,45 +1,61 @@
 import User from '../../models/User';
-import { requireAuth } from '../../services/auth';
+import {
+    requireAuth
+} from '../../services/auth';
 
 export default {
-  signup: async (_, { fullName, ...rest }) => {
-    const [firstName, ...lastName] = fullName.split(' ');
-    try {
-      const user = await User.create({ firstName, lastName, ...rest });
-      return {
-        token: user.createToken(),
-      };
-    } catch (error) {
-      throw error;
-    }
-  },
+    signup: async (_, {
+        fullName,
+        ...rest
+    }) => {
+        const [firstName, ...lastName] = fullName.split(' ');
+        try {
+            const user = await User.create({
+                firstName,
+                lastName,
+                ...rest
+            });
+            return {
+                token: user.createToken(),
+            };
+        } catch (error) {
+            throw error;
+        }
+    },
 
-  login: async (_, { email, password }) => {
-    try {
-      const user = await User.findOne({ email });
+    login: async (_, {
+        email,
+        password
+    }) => {
+        try {
+            const user = await User.findOne({
+                email
+            });
 
-      if (!user) {
-        throw new Error('User not exist!');
-      }
+            if (!user) {
+                throw new Error('User not exist!');
+            }
 
-      if (!user.authenticateUser(password)) {
-        throw new Error('Password not match!');
-      }
+            if (!user.authenticateUser(password)) {
+                throw new Error('Password not match!');
+            }
 
-      return {
-        token: user.createToken(),
-      };
-    } catch (error) {
-      throw error;
-    }
-  },
+            return {
+                token: user.createToken(),
+            };
+        } catch (error) {
+            throw error;
+        }
+    },
 
-  me: async (_, args, { user }) => {
-    try {
-      const me = await requireAuth(user);
-      return me;
-    } catch (error) {
-      throw error;
-    }
-  },
+    me: async (_, args, {
+        user
+    }) => {
+        try {
+            const me = await requireAuth(user);
+            return me;
+        } catch (error) {
+            throw error;
+        }
+    },
 };
