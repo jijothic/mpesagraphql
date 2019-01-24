04441fb4397d22236a6bd957d7bf3af419a5359e
jijothic <george.jijothic@gmail.com>
2019-01-24 12:11:52 +0300

    update b2c resolver

--- src/graphql/resolvers/b2c-resolver.js
+++ src/graphql/resolvers/b2c-resolver.js
@@ -1,28 +1,72 @@
 import Tweet from '../../models/Tweet';
+import {
+    requireAuth
+} from '../../services/auth';
+
 export default {
-    createTweet: (_, args) => Tweet.create(args),
-    getTweet: (_, {
+    getTweet: async (_, {
         _id
-    }) => Tweet.findById(_id),
-    getTweets: () => Tweet.find({}).sort({
-        createdAt: -1
-    }),
-    updateTweet: (_, {
-        _id,
-        ... rest
-    }) => Tweet.findByIdAndUpdate(_id, rest, {
-        new: true
-    }),
-    deleteTweet: async (_, {
-        _id
+    }, {
+        user
     }) => {
         try {
-            await Tweet.findByIdAndRemove(_id);
-            return {
-                message: 'Delete Success!'
-            }
+            await requireAuth(user);
+            return B2C.findById(_id);
         } catch (error) {
             throw error;
         }
-    }
+    },
+
+    getB2Cs: async (_, args, {
+        user
+    }) => {
+        try {
+            await requireAuth(user);
+            return B2C.find({}).sort({
+                createdAt: -1
+            });
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    createB2C: async (_, args, {
+        user
+    }) => {
+        try {
+            await requireAuth(user);
+            return B2C.create(args);
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    updateB2C: async (_, {
+        _id,
+        ...rest
+    }, {
+        user
+    }) => {
+        try {
+            await requireAuth(user);
+            return B2C.findByIdAndUpdate(_id, rest, {
+                new: true
+            });
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    deleteB2C: async (_, {
+        _id
+    }) => {
+        try {
+            await B2C.findByIdAndRemove(_id);
+            return {
+                message: 'Delete Success!',
+            };
+        } catch (error) {
+            throw error;
+        }
+    },
 };
