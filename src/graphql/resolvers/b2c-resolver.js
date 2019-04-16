235900437487d478391d4738b923e0d9496dc1fc
jijothic <george.jijothic@gmail.com>
2019-04-16 14:10:37 +0300

    updated the b2c resolver

--- src/graphql/resolvers/b2c-resolver.js
+++ src/graphql/resolvers/b2c-resolver.js
@@ -1,72 +1,55 @@
-import Tweet from '../../models/Tweet';
-import {
-    requireAuth
-} from '../../services/auth';
+import Tweet from "../../models/Tweet";
+import { requireAuth } from "../../services/auth";
 
 export default {
-    getTweet: async (_, {
-        _id
-    }, {
-        user
-    }) => {
-        try {
-            await requireAuth(user);
-            return B2C.findById(_id);
-        } catch (error) {
-            throw error;
-        }
-    },
+  getTweet: async (_, { _id }, { user }) => {
+    try {
+      await requireAuth(user);
+      return B2C.findById(_id);
+    } catch (error) {
+      throw error;
+    }
+  },
 
-    getB2Cs: async (_, args, {
-        user
-    }) => {
-        try {
-            await requireAuth(user);
-            return B2C.find({}).sort({
-                createdAt: -1
-            });
-        } catch (error) {
-            throw error;
-        }
-    },
+  getB2Cs: async (_, args, { user }) => {
+    try {
+      await requireAuth(user);
+      return B2C.find({}).sort({
+        createdAt: -1
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
 
-    createB2C: async (_, args, {
-        user
-    }) => {
-        try {
-            await requireAuth(user);
-            return B2C.create(args);
-        } catch (error) {
-            throw error;
-        }
-    },
+  createB2C: async (_, args, { user }) => {
+    try {
+      await requireAuth(user);
+      return B2C.create(args);
+    } catch (error) {
+      throw error;
+    }
+  },
 
-    updateB2C: async (_, {
-        _id,
-        ...rest
-    }, {
-        user
-    }) => {
-        try {
-            await requireAuth(user);
-            return B2C.findByIdAndUpdate(_id, rest, {
-                new: true
-            });
-        } catch (error) {
-            throw error;
-        }
-    },
+  updateB2C: async (_, { _id, ...rest }, { user }) => {
+    try {
+      await requireAuth(user);
+      return B2C.findByIdAndUpdate(_id, rest, {
+        new: true
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
 
-    deleteB2C: async (_, {
-        _id
-    }) => {
-        try {
-            await B2C.findByIdAndRemove(_id);
-            return {
-                message: 'Delete Success!',
-            };
-        } catch (error) {
-            throw error;
-        }
-    },
+  deleteB2C: async (_, { _id }) => {
+    try {
+      await B2C.findByIdAndRemove(_id);
+      return {
+        message: "Delete Success!"
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
 };
